c14857e1daf657042940815120aa613c7739a5ce
kianniak <153292392+kianniak@users.noreply.github.com>
2024-02-08 08:08:42 +0000

    riffing.py

--- riffing.py
+++ riffing.py
@@ -82,10 +82,10 @@ def aggframe():
     col1, col2 = st.columns(2, gap="small")
     with col1:
         with st.expander('Company Trait Filters'):
-            company_trait_filters()
+            selected_companies, selected_regions, selected_industries, selected_size = company_trait_filters()
     with col2:
         with st.expander('Company Score Filters'):
-            score_filters()
+            selected_oracle, selected_culture, selected_capacity, selected_conduct, selected_collaboration = score_filters()
     filtered_data = filter_dataframe(df, selected_companies, selected_regions, selected_industries, selected_size, selected_oracle, selected_culture, selected_capacity, selected_conduct, selected_collaboration)
     selected_score =df['Oracle Score']
     st.subheader("Oracle Score Dashboard")
